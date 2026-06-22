8b04147a640cde1fd4d97400e51ce08b3279c0a6
Dominik-Bednorz <bednorzdominik27@gmail.com>
2026-06-22 20:07:52 +0000

    chrome code

--- app.js
+++ app.js
@@ -9,31 +9,24 @@ document.getElementById("playbuttton").addEventListener("click", async () => {
     try {
         await ndef.scan();
 
-        ndef.addEventListener("reading", (event) => {
-            let tagContent = "";
-
-            if (!event.message.records.length) {
-                debug("NFC Error: Keine Records gefunden");
-                return;
-            }
-
-            for (const record of event.message.records) {
-                const textDecoder = new TextDecoder(record.encoding || "utf-8");
-                tagContent += textDecoder.decode(record.data);
-            }
-
-            debug(`NFC Inhalt: ${tagContent}`);
-            window.nfcData = tagContent;
-
-            const player5_test = document.createElement("p");
-            document.getElementById("main").appendChild(player5_test);
-            player5_test.textContent = tagContent;
-        });
-
-    } catch (err) {
-        debug(`NFC Error: ${err}`);
+       ndef.onreading = event => {
+  const message = event.message;
+  for (const record of message.records) {
+    console.log("Record type:  " + record.recordType);
+    console.log("MIME type:    " + record.mediaType);
+    console.log("Record id:    " + record.id);
+    switch (record.recordType) {
+      case "text":
+        // TODO: Read text record with record data, lang, and encoding.
+        break;
+      case "url":
+        // TODO: Read URL record with record data.
+        break;
+      default:
+        // TODO: Handle other records with record data.
     }
-});
+  }
+};
 
 function debug(msg) {
     document.getElementById("debug").innerText += msg + "\n";
