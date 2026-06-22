365642c588b2b18642d678ae1a0b30d33c721c97
Dominik-Bednorz <bednorzdominik27@gmail.com>
2026-06-22 20:23:11 +0000

    mit button

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const ndef = new NDEFReader();
-
 let nfcId = null;
 
-ndef.addEventListener("reading", event => {
-  for (const record of event.message.records) {
+document.getElementById("playbutton").addEventListener("click", async () => {
+  try {
+    await ndef.scan();
 
-    if (record.recordType === "text") {
-      nfcId = Number(new TextDecoder().decode(record.data));
-    }
+    ndef.addEventListener("reading", event => {
+      for (const record of event.message.records) {
 
+        if (record.recordType === "text") {
+          nfcId = Number(new TextDecoder().decode(record.data));
+        }
+
+      }
+    });
+
+  } catch (err) {
+    console.error(err);
   }
 });
