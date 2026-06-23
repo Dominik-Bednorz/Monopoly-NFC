ba40b0249e6e4d24698bc498779ca7a7c7624816
Dominik-Bednorz <bednorzdominik27@gmail.com>
2026-06-23 17:35:53 +0000

    mit JSON gearbeitet

--- app.js
+++ app.js
@@ -1,24 +1,38 @@
-const button = document.getElementById("playbutton");
+const response = await fetch("Daten.json");
+const daten = await response.json(); //JSON wird initalisiert
 
-const ndef = new NDEFReader();
-let nfcID = "";
+const startgame = document.getElementById("playbutton"); //festlegung des Spielstart buttons
 
-button.addEventListener("click", async () => {
+let gameMode = "waiting_for_players"
+
+const ndef = new NDEFReader(); //Element das NFC ließt
+let nfcID = ""; //Varibale für das Auslesen der Tags
+
+document.getElementById("main").style.display = "none"; //nur der Button ist sichtbar
+
+startgame.addEventListener("click", async () => {
+    document.getElementById("startgamescreen").style.display = "none";
+    document.getElementById("main").style.display = "";
+
     await ndef.scan();
 
     ndef.onreading = ({ message }) => {
         const record = message.records[0];
         nfcID = Number(new TextDecoder().decode(record.data));
 
-        debug("gescant:" + nfcID);
+        if (gameMode === "waiting_for_players") {
+            invite_Players(nfcID);
+        }
     };
 });
 
 
-function handle_nfc () {
-    debug("test");
+function invite_Players (i) { //Funktion für das arbeiten mit NFC
+    if (i >= 0 && i <= 4) {
+        return data[i].name
+    }
 };
 
-function debug(msg) {
+function debug(msg) { //statt Console
     document.getElementById("debug").innerText += msg + "\n";
 };
