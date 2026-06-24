4d4d35547e9a8c07fb307597f2e9181209131d0f
Dominik-Bednorz <bednorzdominik27@gmail.com>
2026-06-24 19:36:55 +0000

    invisible class hinzugefügt

--- app.js
+++ app.js
@@ -13,11 +13,11 @@ let gameMode = "waiting_for_players"
 const ndef = new NDEFReader(); //Element das NFC ließt
 let nfcID = ""; //Varibale für das Auslesen der Tags
 
-document.getElementById("main").style.display = "none"; //nur der Button ist sichtbar
+document.getElementById("main").classList.add("invisible");//nur der Button ist sichtbar
 
 startgame.addEventListener("click", async () => {
-    document.getElementById("startgamescreen").style.display = "none";
-    document.getElementById("main").style.display = "";
+    document.getElementById("startgamescreen").classList.add("invisible");
+    document.getElementById("main").classList.remove("invisible");
 
     await ndef.scan();
 
@@ -35,6 +35,7 @@ startgame.addEventListener("click", async () => {
 
 let spielerAnzahl = 0;
 const beigetreteneSpieler = new Set();
+const lobbyFertig;
 
 function invite_Players(id) {
 
@@ -49,13 +50,17 @@ function invite_Players(id) {
     spielerAnzahl++;
 
     if (spielerAnzahl === 2) {
-        const lobbyFertig = document.createElement("button");
+        lobbyFertig = document.createElement("button");
         lobbyFertig.textContent = "Alle Spieler da, starten";
         document.getElementById("main").appendChild(lobbyFertig);
     }
 
     return data[id]?.name + " ist beigetreten";
 }
+
+lobbyFertig.addEventListener("click", () => {
+    lobbyFertig.classList.add("invisible");
+});
 
 function debug(msg) { //statt Console
     document.getElementById("debug").innerText += msg + "\n";
